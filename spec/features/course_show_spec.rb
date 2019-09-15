4863a5a8d04c0ed41fe5b494f4750e3fdf86cf2c
Alec Wells <alec.wells47@gmail.com>
2019-09-15 14:05:33 -0600

    Finish prework

--- spec/features/course_show_spec.rb
+++ spec/features/course_show_spec.rb
@@ -8,11 +8,15 @@ describe 'When user visits course show page' do
     @avery = Student.create!(name:"Avery")
 
     @math = Course.create!(name: "Math")
-
+    @science = Course.create!(name: "Science")
     @course_student_1 = CourseStudent.create!(course_id: @math.id, student_id: @luke.id, grade: 64.0)
     @course_student_4 = CourseStudent.create!(course_id: @math.id, student_id: @avery.id, grade: 94.0)
     @course_student_3 = CourseStudent.create!(course_id: @math.id, student_id: @gus.id, grade: 82.0)
     @course_student_2 = CourseStudent.create!(course_id: @math.id, student_id: @alec.id, grade: 74.0)
+    @course_student_5 = CourseStudent.create!(course_id: @science.id, student_id: @luke.id, grade: 14.0)
+    @course_student_6 = CourseStudent.create!(course_id: @science.id, student_id: @avery.id, grade: 74.0)
+    @course_student_7 = CourseStudent.create!(course_id: @science.id, student_id: @gus.id, grade: 87.0)
+    @course_student_8 = CourseStudent.create!(course_id: @science.id, student_id: @alec.id, grade: 73.0)
   end
 
   it 'they see course info' do
